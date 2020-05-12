4601705e0e675887684adb2fe5fb44f9e8865718
Mike <mikemeducational@gmail.com>
2020-05-12 02:40:17 +0100

    modified:   diskbuilder.sh

--- diskbuilder.sh
+++ diskbuilder.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+##Create needed directory for storing VM storage disks
+
+if [[ -d "$HOME/VMDisks" ]]
+then
+    echo "VMDisks directory exists on your filesystem, doing nothing."
+else
+    mkdir $HOME/VMDisks
+    echo "VMDisks directory created, this can be found under $HOME/VMDisks"
+fi
+
+##Gather needed input from the user
+
 virsh list --all
 
 echo -n "The VM needs to be powered off. In the output above is the VM "shut off" (y/n)? "
@@ -15,22 +27,24 @@ read -p "what size does the disk need to be in mb? (eg 8192 for an 8gb disk): " 
 
 read -p "how should the disk be designated in /dev/? Only answer as you would expect to see in the directory /dev/* IE sda or vdb: " targetdev
 
-dd if=/dev/zero of=/home/mike/VMDisks/$diskname.img bs=1M count=$disksize
+##Build and attach the disks
 
-touch /home/mike/VMDisks/$diskname.xml
+dd if=/dev/zero of=$HOME/VMDisks/$diskname.img bs=1M count=$disksize
+
+touch $HOME/VMDisks/$diskname.xml
 
 echo "<disk type='file' device='disk'> 
    <driver name='qemu' type='raw' cache='none'/> 
    <source file='/home/mike/VMDisks/$diskname.img'/> 
    <target dev='$targetdev'/> 
-</disk>" >>  /home/mike/VMDisks/$diskname.xml
+</disk>" >>  $HOME/VMDisks/$diskname.xml
 
-virsh attach-device --config $vmname /home/mike/VMDisks/$diskname.xml
+virsh attach-device --config $vmname $HOME/VMDisks/$diskname.xml
 
 echo "If no failures occurred then the disk should now be attached."
 echo "To remove the disk run the following command:"
-echo "virsh detach-device --config $vmname /home/mike/VMDisks/$diskname.xml"
-echo "The disk files can then be deleted. They can be found at /home/mike/VMDisks/$diskname.xml and /home/mike/VMDisks/$diskname.img"
+echo "virsh detach-device --config $vmname $HOME/VMDisks/$diskname.xml"
+echo "The disk files can then be deleted. They can be found at $HOME/VMDisks/$diskname.xml and $HOME/VMDisks/$diskname.img"
 
 else 
 echo "VM is not powered off, run "virsh shutdown VMNAME" to power off the VM"
